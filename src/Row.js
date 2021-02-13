204c8eae3d5e4ec7e9a975119695af6ef206b7fa
Priyesh2109 <49438857+Priyesh2109@users.noreply.github.com>
2021-02-13 15:15:01 +0000

    3.0-20mins-fetching the desired results from the api

--- src/Row.js
+++ src/Row.js
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from "react";
 import "./Row.css";
 import axios from "./axios";
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
     }
     fetchData();
-  }, []);
+  }, [fetchUrl]);
+
   return (
     <div className="s">
       <h2>{title} </h2>
