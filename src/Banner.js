605cf5544256dfdb601748ae3a66d1c6b4ffa843
Priyesh2109 <49438857+Priyesh2109@users.noreply.github.com>
2021-02-22 12:02:33 +0000

    13.0- 40mins->styling banner, need to fix responsiveness on mobile

--- src/Banner.js
+++ src/Banner.js
@@ -28,14 +28,11 @@ function Banner() {
       }}
     >
       <div className="banner_contents">
-        <h1 className="banner_title">
+        <h1 className="banner_header">
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
 
-        <div className="banner_buttons">
-          <button className="banner_button">Play</button>
-          <button className="banner_button">My List </button>
-        </div>
+        <h1 className="banner_rating"> rating {movie?.vote_average}</h1>
       </div>
     </header>
   );
